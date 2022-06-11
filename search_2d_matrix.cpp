c48e819d78b648ed94fb972e1e71a7771ed1df3c
Sanmati-Nikam <nikamsanmati.9@gmail.com>
2022-06-11 12:11:04 +0530

    My solution of Search a 2D Matrix 😇

---
+++ search_2d_matrix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        for(int i=0;i<matrix.size();i++){
+            if(matrix[0].size() == 1){
+                if(matrix[i][0] == target)
+                    return true;
+                continue;
+            }
+            if(target>matrix[i][matrix[0].size()-1]){
+                if(i==matrix.size())  break;
+            }
+            else if(target == matrix[i][0] || target == matrix[i][matrix[0].size()-1])
+                return true;
+            
+            else if(target > matrix[i][0] && target < matrix[i][matrix[0].size()-1]){
+                
+                for(int j = 0; j < matrix[0].size(); j++){
+                    if(matrix[i][j] == target)
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+};
