41dac006f706480c7f0e9c52e11b0555e7a7054c
Sanmati Nikam <62207925+Sanmati-Nikam@users.noreply.github.com>
2022-10-12 19:24:01 +0530

    Time: 19 ms (16.61%), Space: 7.3 MB (77.67%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -5,29 +5,21 @@ public:
         cout<<end;
         while(start<end){
             while ((str[start]<48 || (str[start]>57 &&str[start]<65 ) ||
-                (str[start]>90 && str[start]<97) || str[start]>122 ) && start<str.size()){
-                cout<<str[start];
+                (str[start]>90 && str[start]<97) || str[start]>122 ) && start<str.size())
                 start++;
-            }
-            cout<<start;
-            if(start==str.size()) break;
+
+            if(start==str.size()) return true;
             while ((str[end]<48 || (str[end]>57 &&str[end]<65 ) ||
                 (str[end]>90 && str[end]<97) || str[end]>122) && end>0){
                 cout<<str[end];
                 end--;
             }
-            if(end==0) break;
-            cout<<end;
-           // cout<<str[start]<<str[end]<<" ";
-            // if(start<str.size()  && end>0){
-            //     cout<<"in";
-            if(tolower(str[start++])==tolower(str[end--])){
-                cout<<start<<end<<" ";
+            if(end==0) return true;
+            
+            if(tolower(str[start++])==tolower(str[end--]))
                 continue;
-            }
-                else return false;
-            // }
-            // else break;
+    
+            else return false;
             
         }
         return true;
