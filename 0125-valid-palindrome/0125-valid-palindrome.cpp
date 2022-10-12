3db6a5a6f88462ef0ec6fd29c025bbd0e547daac
Sanmati Nikam <62207925+Sanmati-Nikam@users.noreply.github.com>
2022-10-12 19:20:47 +0530

    Time: 207 ms (5.00%), Space: 7.4 MB (77.67%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     bool isPalindrome(string str) {
-        stack<char> s;
-        string r,t;
-        for(int i=0;i<str.length();i++){
-            if((str[i]>=48 && str[i]<=57)||
-        (str[i]>=65 && str[i]<=90)||
-        (str[i]>=97 && str[i]<=122)) {
-            t.push_back(tolower(str[i]));
-            s.push(tolower(str[i]));
-        }
-            else continue;
-        }
-        while(!s.empty()){
-            r.push_back(s.top());
-            s.pop();
-        }
-        cout<<r<<" "<<t;
-        for(int i=0;i<t.size();i++){
-        if(t[i]==r[i])
-            continue;
-        else return false;
+        int start=0,end=str.size()-1;
+        cout<<end;
+        while(start<end){
+            while ((str[start]<48 || (str[start]>57 &&str[start]<65 ) ||
+                (str[start]>90 && str[start]<97) || str[start]>122 ) && start<str.size()){
+                cout<<str[start];
+                start++;
+            }
+            cout<<start;
+            while ((str[end]<48 || (str[end]>57 &&str[end]<65 ) ||
+                (str[end]>90 && str[end]<97) || str[end]>122) && end>0){
+                cout<<str[end];
+                end--;
+            }
+            cout<<end;
+           // cout<<str[start]<<str[end]<<" ";
+            if(start<str.size()  && end>0){
+                cout<<"in";
+            if(tolower(str[start++])==tolower(str[end--])){
+                cout<<start<<end<<" ";
+                continue;
+            }
+                else return false;
+            }
+            else break;
+            
         }
         return true;
     }
