e6cb9adbb10e66b129f2be938987f0abfe2ac0f7
Sanmati Nikam <62207925+Sanmati-Nikam@users.noreply.github.com>
2022-10-13 16:50:29 +0530

    Time: 24 ms (34.35%), Space: 7.5 MB (13.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size()) return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        for(int i=0;i<s.length();i++){
-            if(s[i]!=t[i])
+        // if(s.size()!=t.size()) return false;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // for(int i=0;i<s.length();i++){
+        //     if(s[i]!=t[i])
+        //         return false;
+        // }
+        // return true;
+        map<char,int> m;
+        for(int i=0;i<s.size();i++)
+            m[s[i]]++;
+        for(int i=0;i<t.size();i++){
+            m[t[i]]--;
+        }
+        for(int i=97;i<=122;i++){
+            if(m[i]!=0)
                 return false;
         }
         return true;
