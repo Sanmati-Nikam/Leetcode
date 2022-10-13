8e78085020ccad37310e4507ece5457ff63a5d33
Sanmati Nikam <62207925+Sanmati-Nikam@users.noreply.github.com>
2022-10-13 16:51:53 +0530

    Time: 20 ms (47.84%), Space: 7.4 MB (13.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // if(s.size()!=t.size()) return false;
-        // sort(s.begin(),s.end());
-        // sort(t.begin(),t.end());
-        // for(int i=0;i<s.length();i++){
-        //     if(s[i]!=t[i])
-        //         return false;
-        // }
-        // return true;
         map<char,int> m;
         for(int i=0;i<s.size();i++)
             m[s[i]]++;
