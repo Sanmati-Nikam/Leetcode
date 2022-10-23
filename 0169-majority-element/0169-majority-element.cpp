7747fa7faa48a31e1502827d1ec07b1ad938e953
Sanmati Nikam <62207925+Sanmati-Nikam@users.noreply.github.com>
2022-10-23 14:52:52 +0530

    Time: 23 ms (86.62%), Space: 19.6 MB (60.15%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -5,9 +5,7 @@ public:
         int c=1;
         for(int i=1;i<nums.size();i++){
             if(nums[i]==nums[i-1]){
-                cout<<nums[i]<<c<<endl;
-                c++;
-                if(c==nums.size()/2+1)
+                if(++c==nums.size()/2+1)
                     return nums[i];
             }
             else c=1;
