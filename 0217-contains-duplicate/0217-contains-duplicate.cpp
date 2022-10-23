0cb35131cfcdbf6e9c7cbc45c49095c567b191a2
Sanmati Nikam <62207925+Sanmati-Nikam@users.noreply.github.com>
2022-10-23 15:56:16 +0530

    Time: 258 ms (16.53%), Space: 51.4 MB (65.32%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -10,11 +10,19 @@ public:
 //         }
 //         return false;
         
-        map<int,int> m;
-        for(int i=0;i<nums.size();i++){
-           if(m.find(nums[i])!=m.end())
-               return true;
-            m[nums[i]]++;
+    //     map<int,int> m;
+    //     for(int i=0;i<nums.size();i++){
+    //        if(m.find(nums[i])!=m.end())
+    //            return true;
+    //         m[nums[i]]++;
+    //     }
+    //     return false;
+    // }
+        
+        unordered_set<int> s;
+        for(int i:nums){
+            if(s.insert(i).second==false)
+                return true;
         }
         return false;
     }
