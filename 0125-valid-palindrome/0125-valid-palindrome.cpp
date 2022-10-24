4005812e73dcc6cf225903d1a9fd3f148ebea4e7
Sanmati Nikam <62207925+Sanmati-Nikam@users.noreply.github.com>
2022-10-24 09:48:41 +0530

    Time: 21 ms (14.38%), Space: 7.2 MB (98.67%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     bool isPalindrome(string str) {
         int start=0,end=str.size()-1;
-        cout<<end;
+
         while(start<end){
-            while ((str[start]<48 || (str[start]>57 &&str[start]<65 ) ||
-                (str[start]>90 && str[start]<97) || str[start]>122 ) && start<str.size())
+            while (!isalnum(str[start]) && start<end)
                 start++;
 
             if(start==str.size()) return true;
-            while ((str[end]<48 || (str[end]>57 &&str[end]<65 ) ||
-                (str[end]>90 && str[end]<97) || str[end]>122) && end>0){
-                cout<<str[end];
+            while (!isalnum(str[end]) && start<end)
                 end--;
-            }
+            
             if(end==0) return true;
             
             if(tolower(str[start++])==tolower(str[end--]))
