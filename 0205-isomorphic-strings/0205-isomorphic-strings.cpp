74ae971d617760631f96e227786640d495a7e457
Sanmati Nikam <62207925+Sanmati-Nikam@users.noreply.github.com>
2022-10-26 22:55:42 +0530

    Time: 4 ms (94.14%), Space: 7 MB (73.19%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        map<char,char> ms;
-        map<char,char> mt;
-        for(int i=0;i<s.size();i++){
-            if(ms.find(s[i])!=ms.end()||mt.find(t[i])!=mt.end()){
-                if(ms[s[i]]!=t[i] || mt[t[i]]!=s[i])
-                    return false;
+        // map<char,char> ms;
+        // map<char,char> mt;
+        // for(int i=0;i<s.size();i++){
+        //     if(ms.find(s[i])!=ms.end()||mt.find(t[i])!=mt.end()){
+        //         if(ms[s[i]]!=t[i] || mt[t[i]]!=s[i])
+        //             return false;
+        //     }
+        //     ms[s[i]]=t[i];
+        //     mt[t[i]]=s[i];
+        // }
+        // return true;
+        
+         char mp[128] = {0};
+        char rmp[128] = {0};
+        for (int i = 0; i < s.size(); i++) {
+            if (!mp[s[i]] && !rmp[t[i]]) {
+                mp[s[i]] = t[i];
+                rmp[t[i]] = s[i];
+                continue;
             }
-            ms[s[i]]=t[i];
-            mt[t[i]]=s[i];
+            if (mp[s[i]] != t[i]) return 0;
         }
-        return true;
+        return 1;
     }
 };
