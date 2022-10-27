611a8df3a8dbc4092f84e7fbf611c761abac0d92
Sanmati Nikam <62207925+Sanmati-Nikam@users.noreply.github.com>
2022-10-27 12:38:00 +0530

    Time: 24 ms (17.90%), Space: 7.8 MB (38.35%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -8,17 +8,14 @@ public:
     }
     string reverseVowels(string s) {
         int i=0,k=s.size()-1;
-        //for(int i=0;i<s.size();i++){
         while(i<k){
             if(isVowel(s[i])){
                 while(k>=0 && !isVowel(s[k]))
                     k--;
-                //cout<<s[i]<<" "<<s[k]<<" ";
                 int temp=s[i];
                 s[i]=s[k];
                 s[k--]=temp;
             }
-           // cout<<s<<endl;
             i++;
         }
         return s;
