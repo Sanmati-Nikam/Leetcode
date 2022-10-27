074441abee369b5b1a081868fb0ff0f097710595
Sanmati Nikam <62207925+Sanmati-Nikam@users.noreply.github.com>
2022-10-27 12:00:54 +0530

    Time: 3 ms (48.81%), Space: 6.4 MB (53.96%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -3,23 +3,18 @@ public:
     bool wordPattern(string p, string s) {
         map<char,string> mp;
         map<string,char> ms;
-        int j=0,cnt=0;
+        int j=0;
         for(int i=0;i<p.size();i++){
             string temp="";
             while(j<s.size() && s[j]!=' '){
-                temp+=s[j];
-                j++;
-                cout<<s[j];
+                temp+=s[j++];
             }
-            cnt++;
-            cout<<cnt;
             j++;
             if(temp.empty()||(mp.find(p[i])!=mp.end()||ms.find(temp)!=ms.end()) && (mp[p[i]]!=temp|| ms[temp]!=p[i]))
                 return false;
             mp[p[i]]=temp;
             ms[temp]=p[i];
             temp="";
-            cout<<j;
         }
         if(j-1<s.size()) return false;
         
