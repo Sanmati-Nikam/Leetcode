38105fdd321f36021132ba2005adeeb34128442a
Sanmati Nikam <62207925+Sanmati-Nikam@users.noreply.github.com>
2022-10-27 17:31:28 +0530

    Time: 8 ms (98.41%), Space: 8.6 MB (90.44%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -3,16 +3,12 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         int r[128]={0};
         int m[128]={0};
-        //cout<<ransomNote[0];
         int i=0,j=0;
-        while(i<ransomNote.size()){
-          //  cout<<"ko";
+        while(i<ransomNote.size())
             r[ransomNote[i++]-97]++;
-        }
+        
         while(j<magazine.size())
             m[magazine[j++]-97]++;
-        // cout<<r[ransomNote[0]-97];
-        // cout<<m[magazine[0]-97]<<endl;
         for(i=0;i<ransomNote.size();i++){
             if(r[ransomNote[i]-97]>m[ransomNote[i]-97])
                 return false;
