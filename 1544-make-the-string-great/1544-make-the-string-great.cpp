4cc9699847af9e2ad2cd3a6177a493fa7454ea6a
Sanmati Nikam <62207925+Sanmati-Nikam@users.noreply.github.com>
2022-11-08 20:09:36 +0530

    Time: 3 ms (82.74%), Space: 6.1 MB (97.69%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     string makeGood(string s) {
-        for(int i=s.size()-1;i>0;i--){
+        for(int i=s.size()-1;i>0;i--)
             if(abs(s[i]-s[i-1])==32)
                 s.erase(i-1,2);
-        }
         return s;
     }
 };
