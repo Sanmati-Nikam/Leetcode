04d226af99a92d4369f1a377c648f92f9b1d2457
Sanmati Nikam <62207925+Sanmati-Nikam@users.noreply.github.com>
2022-11-10 10:39:58 +0530

    Time: 486 ms (16.02%), Space: 9.9 MB (93.61%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        for(int i=s.size()-1;i>0;i--){
+            if(abs(s[i]-s[i-1])==0)
+                s.erase(i-1,2);
+        }
+        return s;
+    }
+};
+
+// class Solution {
+// public:
+//     string makeGood(string s) {
+//         for(int i=s.size()-1;i>0;i--){
+//             if(abs(s[i]-s[i-1])==32)
+//                 s.erase(i-1,2);
+//         }
+//         return s;
+//     }
+// };
