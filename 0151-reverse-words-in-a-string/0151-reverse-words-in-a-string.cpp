c9000f238d576b601bab0855560edcb4d649ed44
Sanmati Nikam <62207925+Sanmati-Nikam@users.noreply.github.com>
2022-11-13 14:21:25 +0530

    Time: 16 ms (40.46%), Space: 20.8 MB (29.74%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -8,27 +8,22 @@ public:
        for(int i=0;i<s.size();i++){
            if(s[i]!=' '){
                ans+=s[i];
-               //if(ch!='\0')
                continue;
            }
            if(ans=="")
                continue;
-          //cout<<ans<<" "<<temp<<"p"<<endl;
            if(!temp.empty())
            temp=ans+' '+temp;
            else temp=ans;
-          // cout<<temp;
            ans="";
            cnt++;
            
        }
-        //cout<<cnt;
         if(cnt==0)
             temp=ans;
         else if(ans!=""){
             temp=ans+' '+temp;
         }
-        //cout<<temp;
         return temp;
     }
 };
